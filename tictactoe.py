d3ebc9984748d736039df1b3861be28931c5bdaa
Svetlana_Panamarenka <panamarenka.svetlana@gmail.com>
2022-02-04 13:40:06 -0600

    completed

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -21,20 +22,17 @@ def initial_state():
 def player(board):
     """
     Returns player who has the next turn on a board.
-    The player function should take a board state as input, and return which player’s turn it is (either X or O).
-    In the initial game state, X gets the first move. Subsequently, the player alternates with each additional move.
-    Any return value is acceptable if a terminal board is provided as input (i.e., the game is already over).
     """
     player_x = 0
-    player_0 = 0
-
-    for axis_x in board:
-        for axis_y in board:
-            if axis_x == X:
+    player_o = 0
+    # simply iterate over the given board and calculate how many player_x and player_o are there
+    for y_axis in board:
+        for x_axis in y_axis:
+            if x_axis == X:
                 player_x += 1
-            elif axis_x == O:
-                player_0 += 1
-    if player_x <= player_0:
+            elif x_axis == O:
+                player_o += 1
+    if player_x <= player_o:
         return X
     else:
         return O
@@ -43,53 +41,71 @@ def player(board):
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
-    The actions function should return a set of all of the possible actions that can be taken on a given board.
-    Each action should be represented as a tuple (i, j) where i corresponds to the row of the move (0, 1, or 2) and j corresponds to which cell in the row corresponds to the move (also 0, 1, or 2).
-    Possible moves are any cells on the board that do not already have an X or an O in them.
-    Any return value is acceptable if a terminal board is provided as input.
     """
-    raise NotImplementedError
+
+    possible_actions = set()
+
+    for y, y_axis in enumerate(board):
+        for x, x_axis in enumerate(y_axis):
+            if x_axis == EMPTY:
+                possible_actions.add((y, x))
+
+    return possible_actions
 
 
 def result(board, action):
     """
-    The result function takes a board and an action as input, and should return a new board state, without modifying the original board.
-If action is not a valid action for the board, your program should raise an exception.
-The returned board state should be the board that would result from taking the original input board, and letting the player whose turn it is make their move at the cell indicated by the input action.
-Importantly, the original board should be left unmodified: since Minimax will ultimately require considering many different board states during its computation. This means that simply updating a cell
-in board itself is not a correct implementation of the result function. You’ll likely want to make a deep copy of the board first before making any changes.
+    Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    if len(action) != 2:
+        raise Exception("result function: incorrect action")
+
+    if action[0] < 0 or action[0] > 2 or action[1] < 0 or action[1] > 2:
+        raise Exception("result function: incorrect action value")
+
+    y, x = action[0], action[1]
+
+    board_copy = copy.deepcopy(board)
+
+    if board_copy[y][x] != EMPTY:
+        raise Exception("suggested action has already been taken")
+    else:
+        board_copy[y][x] = player(board)
+
+    return board_copy
 
 
 def winner(board):
     """
-    The winner function should accept a board as input, and return the winner of the board if there is one.
-If the X player has won the game, your function should return X. If the O player has won the game, your function should return O.
-One can win the game with three of their moves in a row horizontally, vertically, or diagonally.
-You may assume that there will be at most one winner (that is, no board will ever have both players with three-in-a-row, since that would be an invalid board state).
-If there is no winner of the game (either because the game is in progress, or because it ended in a tie), the function should return None.
+    Returns the winner of the game, if there is one.
     """
-    for index in range(3):
-        if (board[0][index] == board[1][index] == board[2][index] and board[0][index] != EMPTY):
-            return board[0][index]
-        if (board[index][0] == board[index][1] == board[index][2] and board[index][0] != EMPTY):
-            return board[0][index]
-    if(board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0]) :
+    # Since the board is always 3x3, I believe this approach is reasonable
+    for y in range(3):
+        # Check horizontal lines
+        if (board[y][0] == board[y][1] == board[y][2]) and (board[y][0] != EMPTY):
+            return board[y][0]
+        # check vertical lines
+        if (board[0][y] == board[1][y] == board[2][y]) and (board[0][y] != EMPTY):
+            return board[0][y]
+
+    # Check diagonals
+    if (board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0]) \
+            and board[1][1] != EMPTY:
         return board[1][1]
+
     return None
 
 
 def terminal(board):
     """
-    The terminal function should accept a board as input, and return a boolean value indicating whether the game is over.
-    If the game is over, either because someone has won the game or because all cells have been filled without anyone winning, the function should return True.
-    Otherwise, the function should return False if the game is still in progress.
+    Returns True if game is over, False otherwise.
     """
-    winner_result = winner(board)
-    if winner_result == X or winner_result == O:
+    if winner(board) == X or winner(board) == O:  # check if there is a winner
         return True
-    elif EMPTY not in board[0] and EMPTY not in board[1] and EMPTY not in board[2]:
+    elif EMPTY not in board[0] and EMPTY not in board[1] and EMPTY not in board[2]:  # check if no empty  cells are left
+        return True
+    else:
         return False
 
 
@@ -109,4 +125,62 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    if player(board) == X:
+        score = -math.inf
+        action_to_take = None
+
+        for action in actions(board):
+            min_val = minvalue(result(board, action))
+
+            if min_val > score:
+                score = min_val
+                action_to_take = action
+
+        return action_to_take
+
+    elif player(board) == O:
+        score = math.inf
+        action_to_take = None
+
+        for action in actions(board):
+            max_val = maxvalue(result(board, action))
+
+            if max_val < score:
+                score = max_val
+                action_to_take = action
+
+        return action_to_take
+
+
+def minvalue(board):
+    """
+    Returns the minimum value out of all maximum values
+    """
+
+    if terminal(board):  # if game over, just return the utility of state
+        return utility(board)
+
+    max_value = math.inf  # iterate over the available actions and return the minimum out of all maximums
+    for action in actions(board):
+        max_value = min(max_value, maxvalue(result(board, action)))
+
+    return max_value
+
+
+def maxvalue(board):
+    """
+    Returns the maximum value out of all minimum values
+    """
+
+    if terminal(board):
+        return utility(board)
+
+    min_val = -math.inf
+    for action in actions(board):
+        min_val = max(min_val, minvalue(result(board, action)))
+
+    return min_val
+
